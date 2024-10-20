ef567141b08d53ab784f36639b6c3e74ce93576a
ledjolleshaj <ledjo.lleshaj@gmail.com>
2024-10-20 22:30:33 +0200

    finished login api in be

---
+++ backend/selita/auth.py
@@ -0,0 +1,44 @@
+from rest_framework.response import Response
+from .models import Users
+from rest_framework.decorators import api_view
+import jwt
+import time
+import os
+import dotenv
+from datetime import timedelta, datetime, timezone
+
+dotenv.load_dotenv()
+
+secret = os.getenv("secret")
+algorithm = os.getenv("algorithm")
+
+
+# ======== LOGIN ========
+@api_view(["POST"])
+def login(request):
+    data = request.data
+    user = Users.objects.get(username=data["username"])
+    if user.password == data["password"]:
+
+        token = jwt.encode(
+            {
+                "username": user.username,
+                "exp": datetime.now(timezone.utc) + timedelta(seconds=30),
+            },
+            secret,
+            algorithm=algorithm,
+        )
+        # response has token, user's first name and last name
+        return Response(
+            {
+                "token": token,
+                "first_name": user.firstname,
+                "last_name": user.lastname,
+            }
+        )
+
+    else:
+        return Response(
+            {"error": "Invalid username or password"},
+            status=401,
+        )
