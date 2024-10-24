3eff9ef102a06f4510a452493352c884cda6d2ed
silvergjeka22 <86209628+silvergjeka22@users.noreply.github.com>
2024-10-24 18:34:04 +0200

    adding the paginator

--- frontend/src/app/products-view/products-view.component.ts
+++ frontend/src/app/products-view/products-view.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 interface Product {
   id: number;
@@ -15,9 +18,11 @@ interface Product {
   styleUrls: ['./products-view.component.scss']
 })
 export class ProductsViewComponent implements OnInit {
-  products: Product[] = [];
-  filteredProducts: Product[] = [];
+  dataSource: MatTableDataSource<Product> = new MatTableDataSource();
   displayedColumns: string[] = ['id', 'name', 'category', 'price', 'description'];
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private http: HttpClient) {}
 
@@ -26,17 +31,19 @@ export class ProductsViewComponent implements OnInit {
   }
 
   fetchProducts() {
-                            //  make the link to be 0.0.0.0 for Ledjo
     this.http.get<Product[]>('http://127.0.0.1:8080/selita/products').subscribe(data => {
-      this.products = data;
-      this.filteredProducts = data; // Initialize filteredProducts
+      this.dataSource.data = data;
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     });
   }
 
-  onSearchNameChange(event: any) {
-    const searchTerm = event.target.value.toLowerCase();
-    this.filteredProducts = this.products.filter(product => 
-      product.name.toLowerCase().includes(searchTerm)
-    );
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
   }
 }
