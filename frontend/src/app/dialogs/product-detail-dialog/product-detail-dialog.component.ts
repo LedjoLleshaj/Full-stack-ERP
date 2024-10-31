46109fed2396c06ce5c7f66ae6eebd6bd3d8e46f
silvergjeka22 <86209628+silvergjeka22@users.noreply.github.com>
2024-10-31 17:44:45 +0100

    making dialogs folder

--- frontend/src/app/dialogs/product-detail-dialog/product-detail-dialog.component.ts
+++ frontend/src/app/dialogs/product-detail-dialog/product-detail-dialog.component.ts
@@ -1,6 +1,6 @@
 import { Component, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-import { Product } from '../models/product.model';
+import { Product } from '../../models/product.model';
 
 @Component({
   selector: 'app-product-detail-dialog',
