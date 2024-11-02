c7041d034738f2a5df2bbab9f6207ff5ea944082
ledjolleshaj <ledjo.lleshaj@gmail.com>
2024-11-02 16:30:15 +0100

    removing zoom

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -5,12 +5,4 @@ import { environment } from "../environment/environments";
   selector: "app-root",
   templateUrl: "./app.component.html",
 })
-export class AppComponent {
-  zoomPercentage = 100;
-
-  zoom(direction: number) {
-    const className = document.body.className.replace(` se-zoom-${this.zoomPercentage}`, "");
-    this.zoomPercentage += direction;
-    document.body.className = className + ` se-zoom-${this.zoomPercentage}`;
-  }
-}
+export class AppComponent {}
