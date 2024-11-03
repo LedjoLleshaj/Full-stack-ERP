0f65a659af23b35c74b21a5b5a630b73223c7bb5
ledjolleshaj <ledjo.lleshaj@gmail.com>
2024-11-03 18:23:03 +0100

    implemented token refresh

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -44,7 +44,7 @@ export class LoginComponent implements OnInit {
         const refreshToken = response.refresh_token;
         const userData = atob(token.split(".")[1]);
         localStorage.setItem(LOCAL_STORAGE_KEYS.AUTH_TOKEN, token);
-        localStorage.setItem(LOCAL_STORAGE_KEYS.REFERSH_TOKEN, refreshToken);
+        localStorage.setItem(LOCAL_STORAGE_KEYS.REFRESH_TOKEN, refreshToken);
         localStorage.setItem(LOCAL_STORAGE_KEYS.USERNAME, JSON.parse(userData).username);
         localStorage.setItem(LOCAL_STORAGE_KEYS.FIRST_NAME, response.first_name);
         localStorage.setItem(LOCAL_STORAGE_KEYS.LAST_NAME, response.last_name);
--- frontend/src/app/shared/services/auth-api/auth.interceptor.ts
+++ frontend/src/app/shared/services/auth-api/auth.interceptor.ts
@@ -1,7 +1,8 @@
 // auth.interceptor.ts
 import { Injectable } from "@angular/core";
 import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Observable } from "rxjs";
+import { Observable, of } from "rxjs";
+import { catchError, switchMap, tap } from "rxjs/operators";
 import { AuthApiService } from "./auth-api.service";
 
 @Injectable()
@@ -11,15 +12,38 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const token = this.authService.getToken();
 
+    let authReq = req;
     if (token) {
-      console.log("auth token: interceptor", token); // Log auth token to console
       // Clone the request and add the Authorization header with 'Bearer' prefix
-      const authReq = req.clone({
+      authReq = req.clone({
         headers: req.headers.set("Authorization", `Bearer ${token}`),
       });
-      return next.handle(authReq);
-    } else {
-      return next.handle(req);
     }
+
+    return next.handle(authReq).pipe(
+      catchError((error) => {
+        // If the error is due to an expired token (401 or 403)s
+        if (error.status === 401 || error.status === 403) {
+          // Attempt to refresh the access token
+          return this.authService.refreshToken().pipe(
+            switchMap((newTokenResponse: any) => {
+              // Clone the original request and set the new access token
+              const newAuthReq = req.clone({
+                headers: req.headers.set("Authorization", `Bearer ${newTokenResponse.access}`),
+              });
+              // Retry the original request with the new access token
+              return next.handle(newAuthReq);
+            }),
+            catchError((err) => {
+              // If refreshing the token fails, you may want to log out the user
+              AuthApiService.logout();
+              return of(err); // Return the original error
+            })
+          );
+        } else {
+          return of(error); // If the error is not a 401 or 403, just return the error
+        }
+      })
+    );
   }
 }
