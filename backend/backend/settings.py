1fcea319883b292e16be661f1b88a8e00886963f
ledjolleshaj <ledjo.lleshaj@gmail.com>
2024-11-05 19:57:09 +0100

    implemeted chips instead of buttons for multiple category selections

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,7 +14,7 @@ JWT_ALGORITHM = "HS256"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost"]
 
 # Application definition
 
