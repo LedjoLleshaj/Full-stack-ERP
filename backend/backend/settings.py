4709a0cfeb2d730021c187232f1cbc5144b428a1
ledjolleshaj <ledjo.lleshaj@gmail.com>
2025-04-12 20:18:04 +0200

    refactoring

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -117,7 +117,7 @@ REST_FRAMEWORK = {
 
 # Simple JWT configuration
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=12),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
