ec750cfcaf5d7525b204417c28725d7c68d657e3
ledjolleshaj <ledjo.lleshaj@gmail.com>
2025-04-13 19:24:04 +0200

    Reapply "Merge pull request #3 from LedjoLleshaj/buy-frontend"
    This reverts commit 3431dd7dff03e53da8704b18f09300397aaeac4e.

--- frontend/src/environment/environments.ts
+++ frontend/src/environment/environments.ts
@@ -1,12 +1,13 @@
 export const environment = {
   production: false,
-  apiUrl: "http://127.0.0.1:8080/selita",
+  apiUrl: "http://0.0.0.0:8080/selita",
   login: "/login",
   refreshToken: "/api/token/refresh/",
   addProduct: "/add-product",
   getProducts: "/products",
   getProductCategories: "/product-categories",
   getProductNames: "/product-names",
+  filterByCategories: "/filterbycategories",
   getSales: "/salesinfo",
   productbyid: "/productbyid",
 };
