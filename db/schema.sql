333e1531f02ca4691dff2c55f43ba7b3d4b4b1b4
ledjolleshaj <ledjo.lleshaj@gmail.com>
2025-04-13 17:10:52 +0200

    db update

--- db/schema.sql
+++ db/schema.sql
@@ -63,7 +63,7 @@ CREATE TABLE Restock (
 );
 
 -- Inserting data
-INSERT INTO Users (username, password, email, firstname, lastname, role) VALUES ('admin', 'admin', 'admin@selita_fish.com', 'Admin', 'Admin', 'admin');
+INSERT INTO Users (username, password, email, firstname, lastname, role) VALUES ('admin', 'pbkdf2_sha256$870000$4Q0rcQv0fwttnrAAWvqpfU$vzu7I2egFdnXYvsfrGna0Ee5PO2u5O+XeQ7z1avuyrI=', 'admin@selita_fish.com', 'Admin', 'Admin', 'admin');
 INSERT INTO Product_Categories (category_name) VALUES ('Peshk'), ('Fruta Deti'), ('Gafforre'), ('Kallamar'), ('Midhje'),('Karkaleca'),('Peshk i eger');
 INSERT INTO Product_Names (product_name, category_id) VALUES 
 ('Salmon',1),('Karkaleca',5),('Koce',1),('Midhje',
@@ -81,9 +81,8 @@ INSERT INTO Product (name, category, price, description) VALUES
 ('Karkaleca', 'Karkaleca', 22.00, 'Crab from Adriatic'),
 ('Peshk i eger', 'Peshk i eger', 35.00, 'Wild fish from Adriatic');
 INSERT INTO Inventory (prod_id, quantity) VALUES 
-(1, 100), (2, 100), (3, 100), (4, 100), (5, 100), (6, 100), (7, 100), (8, 100), (9, 100), (10, 100), (11, 100), (12, 100), (13, 100), (14, 100), (15, 100), (16, 100), (17, 100), (18, 100), (19, 100);
-INSERT INTO Sales (prod_id, user_id, quantity) VALUES 
-(1, 1, 10), (2, 1, 5), (3, 1, 20), (4, 1, 15), (5, 1, 8), (6, 1, 12), (7, 1, 7), (8, 1, 9), (9, 1, 11), (10, 1, 6), (11, 1, 14), (12, 1, 13), (13, 1, 4), (14, 1, 3), (15, 1, 2), (16, 1, 1), (17, 1, 0), (18, 1, -1), (19, 1, -2);
+(1, 100), (2, 100), (3, 100), (4, 100), (5, 100), (6, 100), (7, 100), (8, 100), (9, 100), (10, 100), (11, 100);
+INSERT INTO Sales (prod_id, user_id, quantity) VALUES (1, 1, 10), (2, 1, 10), (3, 1, 10), (4, 1, 10), (5, 1, 10), (6, 1, 10), (7, 1, 10), (8, 1, 10), (9, 1, 10), (10, 1, 10), (11, 1, 10), (2, 1, 10), (3, 1, 10), (4, 1, 10), (5, 1, 10), (6, 1, 10), (7, 1, 10), (8, 1, 10), (9, 1, 10);
 INSERT INTO Restock (prod_id, quantity) VALUES 
-(1, 50), (2, 30), (3, 20), (4, 10), (5, 5), (6, 15), (7, 25), (8, 35), (9, 45), (10, 55), (11, 65), (12, 75), (13, 85), (14, 95), (15, 105), (16, 115), (17, 125), (18, 135), (19, 145);
+(1, 50), (2, 30), (3, 20), (4, 10), (5, 5), (6, 15), (7, 25), (8, 35), (9, 45), (10, 55), (11, 65);
 
