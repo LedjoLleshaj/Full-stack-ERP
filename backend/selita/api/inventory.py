6769ccc82e7e3c8f384555782535cb2ad7bb1513
ledjolleshaj <ledjo.lleshaj@gmail.com>
2025-04-27 20:36:00 +0200

    update product add api in inventory

--- backend/selita/api/inventory.py
+++ backend/selita/api/inventory.py
@@ -44,3 +44,76 @@ def getProductsFromInventory(request):
             {"error": "An unexpected error occurred", "details": str(e)},
             status=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
+
+
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated])
+def updateInventory(request, pk):
+    try:
+        inventory = Inventory.objects.get(id=pk)
+        serializer = InventorySerializer(instance=inventory, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except ObjectDoesNotExist:
+        return Response(
+            {"error": "Inventory item not found"},
+            status=status.HTTP_404_NOT_FOUND,
+        )
+    except Exception as e:
+        return Response(
+            {"error": "An unexpected error occurred", "details": str(e)},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
+
+
+@api_view(["PUT"])
+@permission_classes([IsAuthenticated])
+def addProductToInventory(request):
+    try:
+        name = request.data.get("name")
+        category = request.data.get("category")
+        description = request.data.get("description")
+        quantity = request.data.get("quantity")
+        price = request.data.get("price")
+        print("addProductToInventory", name, category, description, quantity, price)
+        if quantity <= 0:
+            return Response(
+                {"error": "Quantity must be greater than zero"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        if price <= 0:
+            return Response(
+                {"error": "Price must be greater than zero"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+        # Check if the product already exists
+        try:
+            product = Product.objects.get(name=name)
+            print("Product already exists", product)
+        except ObjectDoesNotExist:
+            # Create a new product if it doesn't exist
+            product = Product.objects.create(
+                name=name, category=category, description=description, price=price
+            )
+        # Check if the inventory item already exists
+        try:
+            inventory = Inventory.objects.get(prod=product)
+            # Update the existing inventory item's quantity
+            inventory.quantity += quantity
+
+            inventory.save()
+        except ObjectDoesNotExist:
+            # Create a new inventory item if it doesn't exist
+            inventory = Inventory.objects.create(prod=product, quantity=quantity)
+        # Serialize the updated inventory item
+        serializer = InventorySerializer(inventory)
+        return Response(serializer.data)
+    except Exception as e:
+        return Response(
+            {"error": "An unexpected error occurred", "details": str(e)},
+            status=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        )
