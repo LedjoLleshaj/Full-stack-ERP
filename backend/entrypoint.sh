cac827dd75dbef3286125f934a34b03123a9c0dc
ledjolleshaj <ledjo.lleshaj@gmail.com>
2025-05-04 22:17:48 +0200

    using entrypoint for better control

---
+++ backend/entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "Waiting for REDACTED..."
+
+while ! nc -z db 5432; do
+  sleep 1
+done
+
+echo "PostgreSQL started"
+
+# Run migrations
+python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; \
+User = get_user_model(); \
+User.objects.filter(username='adminselita').exists() or \
+User.objects.create_superuser('adminselita', 'admin@selita.com', 'adminpass')" | python manage.py shell
+
+
+exec "$@"
