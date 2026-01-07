f52cc12a7d5db4a5b2bddfdbf8f952e6214b8ca5
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2026-01-07 17:28:33 +0100

    product details component

---
+++ frontend/src/app/views/product-details-view/product-details-view.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Product } from '../../models/product.model';
+import { ProductService } from '../../shared/services/product-api/product.service';
+
+@Component({
+  selector: 'app-product-details-view',
+  templateUrl: './product-details-view.component.html',
+  styleUrls: ['./product-details-view.component.scss']
+})
+export class ProductDetailsViewComponent implements OnInit {
+  product: Product | null = null;
+
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {}
+
+  ngOnInit(): void {
+    const productId = this.route.snapshot.paramMap.get('id');
+    if (productId) {
+      this.productService.getProductById(+productId).subscribe((product: Product) => {
+        this.product = product;
+      });
+    }
+  }
+}
+
