9b7366ecd8cdbc01cc85cc07ed425f09777f5163
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2026-01-16 10:23:53 +0100

    added total amount for cash card and smal style changes

--- frontend/src/app/views/reports-view/reports-view.component.ts
+++ frontend/src/app/views/reports-view/reports-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ReportsApiService, DashboardStats } from "src/app/shared/services/reports-api/reports-api.service";
+import { CurrencyExchangeService, ExchangeRatesResponse } from "src/app/shared/services/currency-exchange/currency-exchange.service";
 
 @Component({
   selector: "app-reports-view",
@@ -15,9 +16,19 @@ export class ReportsViewComponent implements OnInit {
     debt: 0
   };
 
-  constructor(private reportsService: ReportsApiService) {}
+  exchangeRates: { [from: string]: { [to: string]: number } } = {};
+
+  constructor(
+    private reportsService: ReportsApiService,
+    private currencyService: CurrencyExchangeService
+  ) {}
 
   ngOnInit() {
+    // Load exchange rates first, then load stats
+    this.currencyService.getExchangeRates().subscribe(data => {
+      this.exchangeRates = data.rates;
+    });
+
     this.reportsService.getDashboardStats().subscribe(data => {
       this.stats = data;
     });
@@ -29,5 +40,26 @@ export class ReportsViewComponent implements OnInit {
 
   get bankAccounts() {
     return this.stats.accounts.filter(a => a.type === 'BANK');
+  }
+
+  get cashTotalEur(): number {
+    return this.calculateTotalInEur(this.cashAccounts);
+  }
+
+  get bankTotalEur(): number {
+    return this.calculateTotalInEur(this.bankAccounts);
+  }
+
+  private calculateTotalInEur(accounts: { currency: string; balance: number }[]): number {
+    let total = 0;
+    for (const account of accounts) {
+      if (account.currency === 'EUR') {
+        total += account.balance;
+      } else {
+        const rate = this.exchangeRates[account.currency]?.['EUR'] || 1;
+        total += account.balance * rate;
+      }
+    }
+    return total;
   }
 }
