a7b17cca60c8f7c6fc67d028221e1caf65196fb2
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2026-01-17 23:57:01 +0100

    Create index.ts

---
+++ frontend/src/app/shared/pipes/index.ts
@@ -0,0 +1,4 @@
+// Shared Pipes - Export barrel file
+export { PaymentStatusPipe } from './payment-status.pipe';
+export { AlbanianCurrencyPipe } from './albanian-currency.pipe';
+export { AlbanianDatePipe } from './albanian-date.pipe';
