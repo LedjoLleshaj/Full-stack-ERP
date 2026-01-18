a76f2ca2a602a697fb31528e3e19b57cc4a0e75a
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2026-01-18 11:40:30 +0100

    refactoring http params as an abstract object

--- frontend/src/app/shared/services/inventory-api/inventory.service.ts
+++ frontend/src/app/shared/services/inventory-api/inventory.service.ts
@@ -4,13 +4,15 @@ import { Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { Inventory } from "src/app/models/inventory.model";
 
+import { BaseApiService } from "../base-api.service";
+
 @Injectable({
   providedIn: "root",
 })
-export class InventoryService {
-  private apiUrl = environment.apiUrl;
-
-  constructor(private http: HttpClient) {}
+export class InventoryService extends BaseApiService {
+  constructor(http: HttpClient) {
+    super(http);
+  }
 
   updateInventory(data: Inventory): Observable<any> {
     return this.http.put<any>(`${this.apiUrl}${environment.updateInventory}`, data);
