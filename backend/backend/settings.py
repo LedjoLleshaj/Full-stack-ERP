381a057dae964896398c2a4b340c15a31c3d6b9e
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2026-01-19 23:26:18 +0100

    update default api permissions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -126,7 +126,7 @@ REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "selita.authentication.CookieJWTAuthentication",
     ),
-    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.AllowAny",),
+    "DEFAULT_PERMISSION_CLASSES": ("rest_framework.permissions.IsAuthenticated",),
     "DEFAULT_THROTTLE_RATES": {
         "anon": "5/minute",
     },
