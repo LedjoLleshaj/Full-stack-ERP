38aea108a2f9f2308718ceee9e7a4efea3d54326
Ledjo Lleshaj <ledjolleshaj@yahoo.com>
2026-01-30 14:57:57 +0100

    added a simple landing page with some details

---
+++ frontend/src/app/views/landing-page/landing-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent {
+  // Logic can be added here if needed, e.g., smooth scrolling, animations triggering
+}
